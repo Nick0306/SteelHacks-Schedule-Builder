55defcb2a88d78e4c6dc18a173ed3e95a440e9e4
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2022-02-25 20:23:25 -0500

    Person class

--- ScheduleBuilder/src/Person.java
+++ ScheduleBuilder/src/Person.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Person {
     
+    private ArrayList<Schedule> schedules;
+    private String name;
+
+    public Person (String name) {
+        this.name = name;
+    }
+
+    public void addSchedule (Schedule sch) {
+        schedules.add(sch);
+    }
 }
