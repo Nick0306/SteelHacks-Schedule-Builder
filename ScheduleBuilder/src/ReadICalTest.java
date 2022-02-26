df83225ffd5b50865ec4e0b4d536995a300b738d
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2022-02-26 16:49:22 -0500

    ReadICal test method for setWeek

--- ScheduleBuilder/src/ReadICalTest.java
+++ ScheduleBuilder/src/ReadICalTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.joda.time.DateTime;
+
 import biweekly.*;
 import biweekly.component.VEvent;
 
@@ -11,12 +13,11 @@ public class ReadICalTest{
 
     public static void main (String [] args) throws FileNotFoundException, IOException{
 
-        ICalendar ical = Biweekly.parse(new FileReader("ScheduleBuilder/src/Spring Term 2021-2022 (1).ics")).first();
-//        LocalDateTime now = LocalDatetime.now();
+        DateTime now = DateTime.now();
+        readICal nCal = new readICal("ScheduleBuilder/src/Spring Term 2021-2022 (1).ics");
+        nCal.setWeek(now);
+        System.out.println(nCal.weekEnd);
+        System.out.println(nCal.weekStart);
 
-        VEvent event = icals.get(0).getEvents().get(1);
-        String summary = event.getSummary().getValue();
-        System.out.print(event);
-    //    System.out.println(now);
     }
 }
