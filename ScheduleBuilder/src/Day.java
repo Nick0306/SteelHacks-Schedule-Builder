0e1b969cf5772d2774b93abd71116d745efb0a1f
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2022-02-25 20:09:31 -0500

    Attributes for Day and Event classes

--- ScheduleBuilder/src/Day.java
+++ ScheduleBuilder/src/Day.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+
 public class Day {
     
+    private String day;
+    private int dayOfMonth;
+    private String month;
+    private int year;
+    private ArrayList<Event> events;
+
+    public Day (String day, int dayOfMonth, String month, int year) {
+        this.day = day;
+        this.dayOfMonth = dayOfMonth;
+        this.month = month;
+        this.year = year;
+        events = new ArrayList<Events>();
+    }
+
+    private class Event {
+
+        private TimeFrame time;
+        private String title;
+
+        private Event (TimeFrame time, String title) {
+            this.time = time;
+            this.title = title;
+        }
+    }
 }
