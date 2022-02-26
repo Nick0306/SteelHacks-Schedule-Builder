689f25db6ae0aaeb3a77fefd50d9993aeb84e686
Jeffrey Zheng <jeffreyzheng12@gmail.com>
2022-02-25 20:22:16 -0500

    Edited Schedule with an array

--- ScheduleBuilder/src/Schedule.java
+++ ScheduleBuilder/src/Schedule.java
@@ -1,3 +1,16 @@
 public class Schedule {
     
+    //Array of Days of the Week
+    //Sunday is 0 and Saturday is 6
+    Day[] schedOfWeek = new Day[7];
+
+    schedOfWeek[0] = new Day("Sunday");
+    schedOfWeek[1] = new Day("Monday");
+    schedOfWeek[2] = new Day("Tuesday");
+    schedOfWeek[3] = new Day("Wednesday");
+    schedOfWeek[4] = new Day("Thursday");
+    schedOfWeek[5] = new Day("Friday");
+    schedOfWeek[6] = new Day("Saturday");
+
+
 }
