f6ea21f273fb529505afbe9b47fe3fc87462d4bf
Tobias <tbeidlershenk@gmail.com>
2022-02-26 12:48:59 -0500

    Compare algorithm works

--- ScheduleBuilder/src/Schedule.java
+++ ScheduleBuilder/src/Schedule.java
@@ -62,30 +62,47 @@ public class Schedule {
             ArrayList<Event> full = new ArrayList<Event>();
             for (int i = 0; i < days.length; i++) {
                 ArrayList<Event> events = days[i].getEvents();
+                System.out.println(days.length);
                 for (int j = 0; j < events.size(); j++) full.add(events.get(j));
             }
+            Schedule.print(full);
             for (int i = 0; i < full.size()-1; i++) {
                 int idx = i;
-                for (int j = i+1; j < full.size(); j++)
+                for (int j = i+1; j < full.size(); j++) 
                     if (full.get(j).getStart() < full.get(idx).getStart())
                         idx = j;
                 Event temp = full.get(idx);
                 full.set(idx, full.get(i));
                 full.set(i, temp);
+                Schedule.print(full);
             }
+            Schedule.print(full);
             for (int i = 0; i < full.size()-1; i++) {
                 double end = full.get(i).getEnd();
                 double nextStart = full.get(i+1).getStart();
                 double nextEnd = full.get(i+1).getEnd();
                 if (end > nextStart) {
-                    if (end < nextEnd) {
-                        end = nextEnd;
+                    // next event fully enclosed (so we remove it)
+                    if (end > nextEnd) {
+                        full.remove(i+1);
+                    // next event not fully enclosed (so we update the first event)
+                    } else {
+                        full.get(i).setEnd(nextEnd);
                         full.remove(i+1);
                     }
+                    i--;
                 }
             }
+            Schedule.print(full);
             overlayed.add(new Day(full, week[k]));
         }
         return overlayed;
 	}
+
+    public static void print (ArrayList<Event> full) {
+        for (Event e: full) {
+            System.out.print(e.toString() + " ");
+        }
+        System.out.println();
+    }
 }
